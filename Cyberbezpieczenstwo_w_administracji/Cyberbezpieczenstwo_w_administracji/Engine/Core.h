3f16ddf76f06fd752aa1aac12428074f8f1aecf6
juniorekrafcio <juniorekrafcio@interia.pl>
2019-07-15 11:06:24 +0200

    Okno, eventy, czarne tło, czcionka

--- Cyberbezpieczenstwo_w_administracji/Cyberbezpieczenstwo_w_administracji/Engine/Core.h
+++ Cyberbezpieczenstwo_w_administracji/Cyberbezpieczenstwo_w_administracji/Engine/Core.h
@@ -11,6 +11,7 @@ namespace gm
 		const std::string window_name = "Cyberbezpieczeñstwo w administracji";
 		const unsigned int resolution_x = 1280;
 		const unsigned int resolution_y = 720;
+		const unsigned int fps = 60;
 		//////////////////////////////////////////////////////////////////
 	private:
 		Core();
@@ -25,18 +26,16 @@ namespace gm
 		sf::Uint32 last_entered_char;
 		sf::Clock clk;
 		sf::VideoMode resolution;
-		
-		
-
-
-
 	public:
 		~Core();
 
 		static sf::Clock& getClock() { return getSingleton().clk; }
 		static sf::RenderWindow &getWindow() { return *(getSingleton().window); }
 		static sf::Event &getEvent() { return *(getSingleton().ev); }
-		static void resetEvent() { delete getSingleton().ev; getSingleton().ev = new sf::Event; }
 		static sf::VideoMode &getWindowResolution() { return getSingleton().resolution; }
+
+		static void setEnteredChar(sf::Uint32 key) { getSingleton().last_entered_char = key; }
+		static sf::Uint32 getEnteredChar() { return getSingleton().last_entered_char; }
+		static void resetEvent() { delete getSingleton().ev; getSingleton().ev = new sf::Event; }
 	};
 }
--- Cyberbezpieczenstwo_w_administracji/Cyberbezpieczenstwo_w_administracji/main.cpp
+++ Cyberbezpieczenstwo_w_administracji/Cyberbezpieczenstwo_w_administracji/main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
+#include "Engine/Engine.h"
+
+#define ICON_FILEPATH "icon.png"
+#define ICON_SIZE 32
+#define FONT_FILEPATH "fonts/default.ttf"
 
 int main()
 {
-	
+	srand(time(nullptr));
 
+	/*Preparing window*/
+	gm::Assets::LoadIcon(ICON_FILEPATH);
+	if (gm::Assets::getIcon() != nullptr)
+		gm::Core::getWindow().setIcon(ICON_SIZE, ICON_SIZE, gm::Assets::getIcon()->getPixelsPtr());
+
+	/*Loading font*/
+	gm::Assets::LoadFont(FONT_FILEPATH);
+	if (gm::Assets::getFont() == nullptr)
+	{
+		system("pause");
+		exit(EXIT_FAILURE);
+	}
+
+
+	/*Window loop*/
+	while (gm::Core::getWindow().isOpen())
+	{
+		/*Events*/
+		gm::Core::resetEvent();
+
+		while (gm::Core::getWindow().pollEvent(gm::Core::getEvent()))
+		{
+			switch (gm::Core::getEvent().type)
+			{
+			case sf::Event::Closed:
+				gm::Core::getWindow().close();
+				break;
+			case sf::Event::TextEntered:
+				gm::Core::setEnteredChar(gm::Core::getEvent().text.unicode);
+				break;
+			}
+		}
+
+		/*Display graphics*/
+		gm::Core::getWindow().clear(sf::Color::Black);
+		gm::Core::getWindow().display();
+	}
 
 	return EXIT_SUCCESS;
 }
