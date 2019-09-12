e7fa47ec754d64d8d451372d61ef1f9af2f11e78
juniorekrafcio <juniorekrafcio@interia.pl>
2019-09-12 19:33:21 +0200

    Day3

--- Cyberbezpieczenstwo_w_administracji/Cyberbezpieczenstwo_w_administracji/Day_3.h
+++ Cyberbezpieczenstwo_w_administracji/Cyberbezpieczenstwo_w_administracji/Day_3.h
@@ -16,15 +16,42 @@ public:
 
 	OfficeApplicant *ITguy = nullptr;
 
-	//Quests
-	bool finish_fought = false;
+	//Quest1
+	bool open_book = false;
+	bool close_book = false;
+	bool finish_thought = false;
 	bool finish_dialog = false;
 	bool finish_boss_dialog = false;
 	bool uncover_xero = false;
 	bool papers_on_desk = false;
-	bool scan_choice = false;
-	bool scan_choice_2 = false;
-	bool scan_choice_3 = false;
+	bool email_warning = false;
+	//Quest2
+	bool test_scan = false;
+	bool test_scan_2 = false;
+	bool test_scan_3 = false;
+	bool scan_papers = false;
+	bool finished_scans = false;
+	bool finish_communique = false;
+	bool broken_scanner = false;
+	bool fix_scanner = false;
+	bool finish_thought_2 = false;
+	bool finish_boss_dialog_2 = false;
+	bool things_vanish = false;
+
+	//Phone
+	bool phone_pickup = false;
+	bool finish_phone = false;
+	bool finish_IT_dialog = false;
+
+	//Mobile
+	bool mobile_pickup = false;
+	bool aniela_talking = false;
+	bool passed_time = false;
+
+	//Quest3
+	bool finish_boss_dialog_3 = false;
+
+	//BURZA SPALI£A JE¿ELI ANIELA
 
 	Day_3();
 	~Day_3();
